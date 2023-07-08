e933baa4b898d5bde559b60b6fa96746fcb31cff
davor_l <d.lucic4@hotmail.com>
2023-07-08 15:05:58 +0200

    removed carousel due to responsiveness issues, will get back at this later

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -38,97 +38,83 @@ function Home() {
 		<Center>
 			<div>
 				<div className='carousel'>
-					<Carousel
-						slideSize='100%'
-						slidesToScroll={1}
-						withIndicators
-						height={400}
-						withControls={false}
-						align='center'
-						loop
-						speed={1.8}
-						styles={{
-							indicator: {
-								width: rem(12),
-								height: rem(4),
-								transition: 'width 250ms ease',
 
-								'&[data-active]': {
-									width: rem(40),
-								},
-							},
-						}}
-					>
-						<Carousel.Slide><img src={bear1}/></Carousel.Slide>
-						<Carousel.Slide><img src={bear2}/></Carousel.Slide>
-						<Carousel.Slide><img src={bear3}/></Carousel.Slide>
-					</Carousel>
 				</div>
 				<Space h="xl" />
 				<div className={'next'}>
-					<Card shadow={'sm'} radius={'md'} mx={'lg'}>
-						<Card.Section>
-							<AspectRatio ratio={2048/1357}>
-								<Image
-									src={beach}
-									height={'100%'}
-									alt={'beach'}
-									fit={'fill'}
-								/>
-							</AspectRatio>
-							<Group position={'apart'} m={'md'}>
-								<Text weight={500}>Sommerferien!</Text>
-								<Badge color={'green'} variant={'light'}>
-									Info
-								</Badge>
-							</Group>
-							<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-								Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!
-							</Text>
-						</Card.Section>
-					</Card>
-					<Card shadow={'sm'} radius={'md'} mx={'lg'}>
-						<Card.Section>
-							<AspectRatio ratio={2048/1357}>
-								<Image
-									src={beach}
-									height={'100%'}
-									alt={'beach'}
-									fit={'fill'}
-								/>
-							</AspectRatio>
-							<Group position={'apart'} m={'md'}>
-								<Text weight={500}>Sommerferien!</Text>
-								<Badge color={'green'} variant={'light'}>
-									Info
-								</Badge>
-							</Group>
-							<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-								Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!
-							</Text>
-						</Card.Section>
-					</Card>
-					<Card shadow={'sm'} radius={'md'} mx={'lg'}>
-						<Card.Section>
-							<AspectRatio ratio={2048/1357}>
-								<Image
-									src={beach}
-									height={'100%'}
-									alt={'beach'}
-									fit={'fill'}
-								/>
-							</AspectRatio>
-							<Group position={'apart'} m={'md'}>
-								<Text weight={500}>Sommerferien!</Text>
-								<Badge color={'green'} variant={'light'}>
-									Info
-								</Badge>
-							</Group>
-							<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-								Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!
-							</Text>
-						</Card.Section>
-					</Card>
+					<MantineProvider>
+						<Grid justify={'center'} align={'center'}>
+							<Grid.Col md={4} sm={6}>
+								<Card shadow={'sm'} radius={'md'}>
+									<Card.Section>
+										<AspectRatio ratio={2048/1357}>
+											<Image
+												src={beach}
+												height={'100%'}
+												alt={'beach'}
+												fit={'fill'}
+											/>
+										</AspectRatio>
+										<Group position={'apart'} m={'md'}>
+											<Text weight={500}>Sommerferien!</Text>
+											<Badge color={'green'} variant={'light'}>
+												Info
+											</Badge>
+										</Group>
+										<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
+											Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!
+										</Text>
+									</Card.Section>
+								</Card>
+							</Grid.Col>
+							<Grid.Col md={4} sm={6}>
+								<Card shadow={'sm'} radius={'md'}>
+									<Card.Section>
+										<AspectRatio ratio={2048/1357}>
+											<Image
+												src={beach}
+												height={'100%'}
+												alt={'beach'}
+												fit={'fill'}
+											/>
+										</AspectRatio>
+										<Group position={'apart'} m={'md'}>
+											<Text weight={500}>Sommerferien!</Text>
+											<Badge color={'green'} variant={'light'}>
+												Info
+											</Badge>
+										</Group>
+										<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
+											Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!
+										</Text>
+									</Card.Section>
+								</Card>
+							</Grid.Col>
+							<Grid.Col md={4} sm={6}>
+								<Card shadow={'sm'} radius={'md'}>
+									<Card.Section>
+										<AspectRatio ratio={2048/1357}>
+											<Image
+												src={beach}
+												height={'100%'}
+												alt={'beach'}
+												fit={'fill'}
+											/>
+										</AspectRatio>
+										<Group position={'apart'} m={'md'}>
+											<Text weight={500}>Sommerferien!</Text>
+											<Badge color={'green'} variant={'light'}>
+												Info
+											</Badge>
+										</Group>
+										<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
+											Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!
+										</Text>
+									</Card.Section>
+								</Card>
+							</Grid.Col>
+						</Grid>
+					</MantineProvider>
 				</div>
 				<Space h="xl" />
 				<div className='sponsor-images grid'>
