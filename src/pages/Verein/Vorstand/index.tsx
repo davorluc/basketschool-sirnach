30b435cf6495bb1ba09a926f353d2f6ae28f8e5c
davorluc <d.lucic4@hotmail.com>
2023-08-13 15:28:57 +0200

    added U10 and U14 boys and girls

--- src/pages/Verein/Vorstand/index.tsx
+++ src/pages/Verein/Vorstand/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+// Mantine imports
+import {
+    Space,
+    Box,
+    Card,
+    Grid,
+    Text,
+    Title
+} from '@mantine/core'
 
 function Vorstand () {
     return (
         <div>
-            <h2>Vorstand</h2>
+            <div className={'main'}>
+                <Space h={'xl'}/>
+                <Space h={'xl'}/>
+                <Title c={'#189940'} order={1} mt={'xl'} mx={'7%'}>Vorstand</Title>
+            </div>
         </div>
     );
 }
