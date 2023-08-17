810de3f3c6abe80ead48e6113d086a716db930f9
FadilSmajilbasic <smajilbaicfadil@gmail.com>
2023-08-17 14:01:07 +0200

    feat(registration form): :lipstick: Added missing fields to form
    Added additional fields that were present in the paper form
    
    #1

--- src/pages/Verein/Neumitglied/index.tsx
+++ src/pages/Verein/Neumitglied/index.tsx
@@ -2,12 +2,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { Title, Text, Space, Grid, NumberInput } from "@mantine/core";
-import { TextInput, Checkbox, Button, Group } from "@mantine/core";
+import { TextInput, Checkbox, Button, Group, Radio } from "@mantine/core";
 
-import { DateInput } from '@mantine/dates';
+import { DateInput } from "@mantine/dates";
 import { useForm } from "@mantine/form";
+import { useState } from "react";
+
+function underAgeValidate(birthday: Date) {
+    const age = ~~((Date.now() - birthday.getTime()) / 31557600000);
+    return age > 18;
+}
 
 function Neumitglied() {
+    const [date, setDateValue] = useState<Date | null>(null);
+
     const form = useForm({
         initialValues: {
             name: "",
@@ -16,15 +24,28 @@ function Neumitglied() {
             email: "",
             phone: "",
             gdpr: false,
+            address: {
+                street: "",
+                number: undefined,
+                zip: undefined,
+                city: "",
+            },
+            ahv: undefined,
+            sex: undefined,
+            socialPermission: false,
+            supervisor: {
+                name: "",
+                surname: "",
+                phone: "",
+                email: "",
+            },
         },
 
         validate: {
             email: (value) => (/^\S+@\S+$/.test(value) ? null : "Invalid email"),
-            name: (value) => (value.length < 3 ? 'Der Name ist nicht valide' : null),
-            surname: (value) => (value.length < 3 ? 'Der Nachname ist nicht valide' : null),
-
-
-          },
+            name: (value) => (value.length < 3 ? "Der Name ist nicht valide" : null),
+            surname: (value) => (value.length < 3 ? "Der Nachname ist nicht valide" : null),
+        },
     });
 
     return (
@@ -45,27 +66,53 @@ function Neumitglied() {
                     </Text>
                 </div>
                 <div className={"page-content"}>
-                    <Grid mx={"7%"} my={"xl"} gutter={"xl"}>
-                        <Grid.Col sm={6}>
-                            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                    <form onSubmit={form.onSubmit((values) => console.log(values))}>
+                        <Grid mx={"7%"} my={"xl"} grow>
+                            <Grid.Col sm={6} span={2}>
                                 <TextInput withAsterisk label="Vorname" placeholder="Marcus" {...form.getInputProps("name")} />
                                 <TextInput withAsterisk label="Name" placeholder="Aurelius" {...form.getInputProps("surname")} />
-                                <DateInput withAsterisk valueFormat="YYYY.MM.DD" label="Geburtsdatum" placeholder="15.7.2007" value={...form.getInputProps("birthdate")} onChange={} />
-                                <TextInput withAsterisk label="Email" placeholder="marcus.aurelius@gmail.com" {...form.getInputProps("email")} />
-                                <NumberInput label="Nummer" placeholder="0795648977" hideControls {...form.getInputProps("phone")} />
-
-                                <Checkbox
-                                    mt="md"
-                                    label="Ich akzeptiere die Datenschutzbestimmungen."
-                                    {...form.getInputProps("gdpr", { type: "checkbox" })}
+                                <DateInput
+                                    withAsterisk
+                                    valueFormat="DD.MM.YYYY"
+                                    label="Geburtsdatum"
+                                    placeholder="15.7.2007"
+                                    // onChange={(value: Date) => {
+                                    //     setDateValue(value);
+                                    //     if (underAgeValidate(value)) {
+                                    //         console.log("under age");
+                                    //     }
+                                    // }}
                                 />
-
-                                <Group position="right" mt="md">
-                                    <Button type="submit">Submit</Button>
-                                </Group>
-                            </form>
-                        </Grid.Col>
-                    </Grid>
+                                <TextInput withAsterisk label="Email" placeholder="marcus.aurelius@gmail.com" {...form.getInputProps("email")} />
+                                <NumberInput label="Telefonnummer" placeholder="0795648977" hideControls {...form.getInputProps("phone")} />
+                            </Grid.Col>
+                            <Grid.Col sm={6} span={2}>
+                                <TextInput withAsterisk label="Strasse" placeholder="Hauptstrasse" {...form.getInputProps("address.street")} />
+                                <NumberInput withAsterisk label="Nummer" placeholder="12" hideControls {...form.getInputProps("address.number")} />
+                                <NumberInput withAsterisk label="PLZ" placeholder="8000" hideControls {...form.getInputProps("address.zip")} />
+                                <TextInput withAsterisk label="Ort" placeholder="Zürich" {...form.getInputProps("address.city")} />
+                                <NumberInput label="AHV Nummer" placeholder="756.1234.5678.97" hideControls {...form.getInputProps("ahv")} />
+                                <Radio.Group name="sex" label="Geschlecht" withAsterisk>
+                                    <Group mt="xs">
+                                        <Radio value={0} label="M" />
+                                        <Radio value={1} label="W" />
+                                    </Group>
+                                </Radio.Group>
+                            </Grid.Col>
+                            {/*Place for supervisor data
+                            <Grid.Col sm={6} span={2}></Grid.Col> */}
+                        </Grid>
+                        <Checkbox mx={"lg"} mt="md" label="Ich akzeptiere die Datenschutzbestimmungen." {...form.getInputProps("gdpr", { type: "checkbox" })} />
+                        <Checkbox
+                            mt="md"
+                            mx={"lg"}
+                            label="Ich bin damit einverstanden, dass Fotos von mir auf der Website und in den sozialen Medien veröffentlicht werden."
+                            {...form.getInputProps("socialPermission", { type: "checkbox" })}
+                        />
+                        <Group position="right" mt="md" mx="lg">
+                            <Button type="submit">Submit</Button>
+                        </Group>
+                    </form>
                 </div>
             </div>
         </div>
