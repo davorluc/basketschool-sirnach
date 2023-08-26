2bf42a1862a7a492e6f3e46f6c3701198ecff8a4
davorluc <d.lucic4@hotmail.com>
2023-08-26 22:51:39 +0200

    added trainers to overview

--- src/pages/Verein/Trainer/index.tsx
+++ src/pages/Verein/Trainer/index.tsx
@@ -1,9 +1,67 @@
-import React from "react";
+// Mantine imports
+import {
+    Title,
+    Image,
+    Group,
+    Stack,
+    Space,
+    Text,
+    Badge,
+    AspectRatio,
+    Grid,
+    Card
+} from '@mantine/core'
+
+// Image imports
+import miki from '../../../media/portraits/miki.jpeg'
+import alen from '../../../media/portraits/alen.jpeg'
+import ersun from '../../../media/portraits/ersun.jpeg'
+import nef from '../../../media/portraits/nef.jpeg'
+import davor from '../../../media/portraits/davor.jpeg'
 
 function Trainer () {
+    const trainers = [
+    { image: miki, name: 'Miki Manojlovic', selection: 'U18 Herren', email: '*email*', tel: '+41799533413' },
+    { image: alen, name: 'Alen Kruljac', selection: 'U10 Mix/Herren 4. Liga', email: '*email*', tel: '+41792871834' },
+    { image: ersun, name: 'Ersun Luma', selection: 'U18 Herren', email: '*email*', tel: '+41791276850' },
+    { image: nef, name: 'Nefajla Fata', selection: 'U14 Damen', email: '*email*', tel: '+41789107186' },
+    { image: davor, name: 'Davor Lucic', selection: 'U14 Herren/U16 Herren', email: 'd.lucic4@hotmail.com', tel: '+41791795801' },
+    ]
+
+    const renderTrainers = trainers.map((trainer) => (
+        <Grid.Col sm={6} md={4} xl={3}>
+            <Card shadow='md' radius='md' mb='xl'>
+                <Card.Section>
+                    <AspectRatio ratio={6/8}>
+                        <Image
+                            src={trainer.image}
+                        />
+                    </AspectRatio>
+                    <Group position='apart' mx='md' my='xs'>
+                        <Title c='#189940' order={3}>{trainer.name}</Title>
+                        <Badge color='green' variant='light'>{trainer.selection}</Badge>
+                    </Group>
+                    <Stack>
+                        <Text c='dimmed' component='a' href={`mailto:${trainer.email}`} mx='lg'>{trainer.email}</Text>
+                        <Text c='dimmed' component='a' href={`tel:${trainer.tel}`} mx='lg' mb='md'>{trainer.tel}</Text>
+                    </Stack>
+                </Card.Section>
+            </Card>
+        </Grid.Col>
+        ));
+
     return (
         <div>
-            <h2>Trainer/innen</h2>
+            <div className="main">
+                <Space h='xl'/>
+                <Space h='xl'/>
+                <Title order={1} c='#189940' my='xl' mx='7%'>Trainer/Innen</Title>
+                <div className='trainers-grid'>
+                    <Grid gutter={30} mx='8%'>
+                    {renderTrainers}
+                    </Grid>
+                </div>
+            </div>
         </div>
     );
 }
