3b11a20db93a52cfb2d4a212e8a5cbaa5f50aed6
davorluc <d.lucic4@hotmail.com>
2023-09-11 12:13:12 +0200

    refactored code for next up events with map

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -43,10 +43,43 @@ import logo from '../../media/wil_basktetball_small.png'
 // Stylesheet imports here
 import './styles.css';
 import 'aos/dist/aos.css'
+import { render } from 'react-dom';
 
 
 
 function Home() {
+
+	const upNextData = [
+		{ image: first, title: 'Sommerferien!', datetime: '10.07.2023 - 14.08.2023', content: 'Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!' },
+		{ image: second, title: 'Delegiertenversammlung', datetime: '09.06.2023 19:30', content: 'Die jährliche Delegiertenversammlung findet wieder statt! Es erwarten euch Infos bzgl. zur nächsten Saison, Administratives, Budget, Preisverleihung und ein Apéro!'},
+		{ image: third, title: 'Aufstiegsspiel Herren 1!', datetime: '07.05.2023 13:00', content: 'Unsere Herrenmannschaft hat die Möglichkeit, Ihre äusserst erfolgreiche Saison mit einem krönenden Abschluss zu verzieren. In der Kanti Wil kommt es zum Rückspiel gegen Wohlen Basket. Wir brauchen so viel unterstützung wie möglich!' }
+	];
+
+	const renderUpNextData = upNextData.map((event) => (
+		<Grid.Col md={4} sm={6}>
+			<Card shadow={'sm'} radius={'md'}>
+				<Card.Section>
+					<AspectRatio ratio={2048/1357}>
+						<Image
+							src={event.image}
+							height={'100%'}
+							alt={'first'}
+							fit={'fill'}
+						/>
+					</AspectRatio>
+					<Group position={'apart'} m={'md'}>
+						<Text weight={500}>{event.title}</Text>
+						<Badge color={'green'} variant={'light'}>
+							{event.datetime}
+						</Badge>
+					</Group>
+					<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
+						{event.content}
+					</Text>
+				</Card.Section>
+			</Card>
+		</Grid.Col>
+	));
 
 	useEffect(() => {
 		AOS.init({ duration: 1000 })
@@ -167,77 +200,9 @@ function Home() {
                         <Text c='white' mx='7%' mb='xl' size='xl'>Selbst an Spannung mangelt es nicht, denn in unserem "Kommende Termine" Abschnitt verpassen Sie nie wieder ein aufregendes Basketballspiel oder wichtige administrative Veranstaltungen. Halten Sie sich auf dem Laufenden über die nächsten Höhepunkte, die unser Basketballverein zu bieten hat – sei es ein mitreißendes Heimspiel oder eine wichtige Vereinssitzung. Von packenden Auswärtsbegegnungen auf dem Spielfeld bis hin zu bedeutenden organisatorischen Ereignissen neben dem Spielfeld – hier finden Sie alle Termine, die Sie benötigen, um sowohl Teil unserer leidenschaftlichen Fan-Gemeinschaft als auch informiert über die Vereinsangelegenheiten zu sein. Seien Sie bereit, unsere Mannschaft anzufeuern und die Zukunft des Vereins mitzugestalten. Verpassen Sie keine Gelegenheit mehr, die Spannung des Sports und die Bedeutung der Vereinsarbeit hautnah zu erleben. Schauen Sie regelmäßig vorbei, um sicherzustellen, dass Sie keine der kommenden Veranstaltungen, sei es sportlich oder administrativ, verpassen.</Text>
                         </div>
 					<div className={'next-content'}>
-							<Grid justify={'center'} align={'center'} mx={'lg'} pb='xl'>
-								<Grid.Col md={4} sm={6}>
-									<Card shadow={'sm'} radius={'md'}>
-										<Card.Section>
-											<AspectRatio ratio={2048/1357}>
-												<Image
-													src={first}
-													height={'100%'}
-													alt={'first'}
-													fit={'fill'}
-												/>
-											</AspectRatio>
-											<Group position={'apart'} m={'md'}>
-												<Text weight={500}>Sommerferien!</Text>
-												<Badge color={'green'} variant={'light'}>
-													10.07.2023 - 14.08.2023
-												</Badge>
-											</Group>
-											<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-												Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!
-											</Text>
-										</Card.Section>
-									</Card>
-								</Grid.Col>
-								<Grid.Col md={4} sm={6}>
-									<Card shadow={'sm'} radius={'md'}>
-										<Card.Section>
-											<AspectRatio ratio={2048/1357}>
-												<Image
-													src={second}
-													height={'100%'}
-													alt={'second'}
-													fit={'fill'}
-												/>
-											</AspectRatio>
-											<Group position={'apart'} m={'md'}>
-												<Text weight={500}>Delegiertenversammlung</Text>
-												<Badge color={'green'} variant={'light'}>
-													09.06.2023 19:30
-												</Badge>
-											</Group>
-											<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-												Die jährliche Delegiertenversammlung findet wieder statt! Es erwarten euch Infos bzgl. zur nächsten Saison, Administratives, Budget, Preisverleihung und ein Apéro
-											</Text>
-										</Card.Section>
-									</Card>
-								</Grid.Col>
-								<Grid.Col md={4} sm={6} py='xl'>
-									<Card shadow={'sm'} radius={'md'}>
-										<Card.Section>
-											<AspectRatio ratio={2048/1357}>
-												<Image
-													src={third}
-													height={'100%'}
-													alt={'third'}
-													fit={'fill'}
-												/>
-											</AspectRatio>
-											<Group position={'apart'} m={'md'}>
-												<Text weight={500}>Aufstiegsspiel Herren 1!</Text>
-												<Badge color={'green'} variant={'light'}>
-													07.05.2023 13:00
-												</Badge>
-											</Group>
-											<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-												Unsere Herrenmannschaft hat die Möglichkeit, Ihre äusserst erfolgreiche Saison mit einem krönenden Abschluss zu verzieren. In der Kanti Wil kommt es zum Rückspiel gegen Wohlen Basket. Wir brauchen so viel unterstützung wie möglich!
-											</Text>
-										</Card.Section>
-									</Card>
-								</Grid.Col>
-							</Grid>
+						<Grid justify={'center'} align={'center'} mx={'lg'} pb='xl'>
+							{renderUpNextData}				
+						</Grid>
 					</div>
                     </div>
                     <Divider my='xl' mx='7%'/>
