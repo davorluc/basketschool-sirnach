9dcb8f9918a43eef376812684b7290bbd9d9875c
davorluc <d.lucic4@hotmail.com>
2024-07-05 21:42:11 +0200

    added coaches

--- src/pages/Verein/Trainer/index.tsx
+++ src/pages/Verein/Trainer/index.tsx
@@ -8,28 +8,23 @@ import {
     Space,
     Text,
     Badge,
+    Box,
     AspectRatio,
     Grid,
     Card
 } from '@mantine/core'
 
 // Image imports
-import miki from '../../../media/portraits/miki.jpeg'
-import alen from '../../../media/portraits/alen.jpeg'
-import ersun from '../../../media/portraits/ersun.jpeg'
-import nef from '../../../media/portraits/nef.jpeg'
 import davor from '../../../media/portraits/davor.jpeg'
+import placeholder from '../../../media/placeholder_person.jpeg'
 
 // Icon imports
 import { Phone, Mail } from 'tabler-icons-react'
 
 function Trainer () {
     const trainers = [
-    { image: miki, name: 'Miodrag Manojlovic', selection: 'U18 Herren', email: '*email*', tel: '+41799533413' },
-    { image: alen, name: 'Alen Kruljac', selection: 'U10 Mix/Herren 4. Liga', email: '*email*', tel: '+41792871834' },
-    { image: ersun, name: 'Ersun Luma', selection: 'U18 Herren', email: '*email*', tel: '+41791276850' },
-    { image: nef, name: 'Nefajla Fata', selection: 'U14 Damen', email: '*email*', tel: '+41789107186' },
-    { image: davor, name: 'Davor Lucic', selection: 'U14 Herren/U16 Herren', email: 'd.lucic4@hotmail.com', tel: '+41791795801' },
+    { image: davor, name: 'Davor', selection: 'Knaben/Mädchen', email: 'd.lucic4@hotmail.com', tel: '+41791795801' },
+    { image: placeholder, name: 'Thomas', selection: 'Knaben/Mädchen', email: '*email*', tel: '*tel*' },
     ]
 
     const renderTrainers = trainers.map((trainer) => (
@@ -42,12 +37,12 @@ function Trainer () {
                         />
                     </AspectRatio>
                     <Group position='apart' mx='md' my='xs'>
-                        <Title c='#189940' order={2}>{trainer.name}</Title>
-                        <Badge color='green' variant='light'>{trainer.selection}</Badge>
+                        <Title c='#1B1A55' order={2}>{trainer.name}</Title>
+                        <Badge color='blue' variant='light'>{trainer.selection}</Badge>
                     </Group>
                     <Stack spacing='xs' mt='lg'>
-                        <Text c='dimmed' component='a' href={`mailto:${trainer.email}`} mx='lg'><ThemeIcon mx='sm' color='green' variant='light'><Mail /></ThemeIcon>{trainer.email}</Text>
-                        <Text c='dimmed' component='a' href={`tel:${trainer.tel}`} mx='lg' mb='md'><ThemeIcon mx='sm' color='green' variant='light'><Phone /></ThemeIcon>{trainer.tel}</Text>
+                        <Text c='dimmed' component='a' href={`mailto:${trainer.email}`} mx='lg'><ThemeIcon mx='sm' color='blue' variant='light'><Mail /></ThemeIcon>{trainer.email}</Text>
+                        <Text c='dimmed' component='a' href={`tel:${trainer.tel}`} mx='lg' mb='md'><ThemeIcon mx='sm' color='blue' variant='light'><Phone /></ThemeIcon>{trainer.tel}</Text>
                     </Stack>
                 </Card.Section>
             </Card>
@@ -59,9 +54,16 @@ function Trainer () {
             <div className="main">
                 <Space h='xl'/>
                 <Space h='xl'/>
-                <Title order={1} c='#189940' my='xl' mx='7%'>Trainer/Innen</Title>
+                <Box mx={'7%'} mt={'4%'} sx={() => ({
+                    backgroundColor: '#1B1A55',
+                    borderRadius: '7px'
+                })}>
+                    <Title order={1} mt={'xl'} mx={'xl'} c={'white'}>
+                        Trainer 
+                    </Title>
+                </Box>
                 <div className='trainers-grid'>
-                    <Grid gutter={30} mx='8%'>
+                    <Grid gutter={30} mx='8%' my={'xl'}>
                     {renderTrainers}
                     </Grid>
                 </div>
