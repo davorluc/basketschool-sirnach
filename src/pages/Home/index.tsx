6110cae7700bb8a4df617faaead59d1e39dab8db
davorluc <d.lucic4@hotmail.com>
2024-08-04 14:05:01 +0200

    new sponsors
    changed old sponsors to new sponors

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,21 +3,21 @@ import { useEffect, useRef } from 'react';
 
 // Mantine Imports here
 import {
-	Grid,
-	Center,
-	Divider,
-    Button,
-	Card,
-	Image,
-    Space,
-    createStyles,
-    Paper,
-	Text,
-	Badge,
-	Group,
-	rem,
-	Title,
-	AspectRatio
+  Grid,
+  Center,
+  Divider,
+  Button,
+  Card,
+  Image,
+  Space,
+  createStyles,
+  Paper,
+  Text,
+  Badge,
+  Group,
+  rem,
+  Title,
+  AspectRatio
 } from "@mantine/core";
 import { Carousel } from '@mantine/carousel';
 import Autoplay from "embla-carousel-autoplay";
@@ -32,13 +32,14 @@ import whole from '../../media/teams/all.jpeg'
 import first from '../../media/beachphoto.jpg'
 import second from '../../media/DV.jpg'
 import third from '../../media/teams/senioren.jpg'
-import stilmat from '../../media/sponsors/stilmat-logo-ch.png';
-import hederavita from '../../media/sponsors/hederavita.png';
-import tgazajug from '../../media/sponsors/tga_za_jug.jpeg';
-import munishi from '../../media/sponsors/munishi_logo.jpeg';
-import konsens from '../../media/sponsors/konsens_logo.png';
-import dieMobiliar from '../../media/sponsors/die_mobiliar.jpeg';
 import logo from '../../media/wil_basktetball_small.png'
+import hostpoint from '../../media/sponsors/logo_hostpoint_orig.png';
+import velopeter from '../../media/sponsors/logo_velopeter_ohne_weiss.png';
+import apotheke from '../../media/sponsors/logo_apotheke.png';
+import swissInsurance from '../../media/sponsors/Logo_swissinsuranceag_04.jpg';
+import raiffeisen from '../../media/sponsors/logo_raiffeisen_02.gif';
+import fitness4all from '../../media/sponsors/logo_fitness4all_02.jpg';
+
 
 // Stylesheet imports here
 import './styles.css';
@@ -49,125 +50,127 @@ import { render } from 'react-dom';
 
 function Home() {
 
-	const upNextData = [
-		{ image: first, title: 'Sommerferien!', datetime: '05.07.2024 - 12.08.2024', content: 'Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!' },
-		{ image: first, title: 'Sommerferien!', datetime: '05.07.2024 - 12.08.2024', content: 'Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!' },
-		{ image: first, title: 'Sommerferien!', datetime: '05.07.2024 - 12.08.2024', content: 'Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!' },
-	];
+  const upNextData = [
+    { image: first, title: 'Sommerferien!', datetime: '05.07.2024 - 12.08.2024', content: 'Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!' },
+    { image: first, title: 'Sommerferien!', datetime: '05.07.2024 - 12.08.2024', content: 'Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!' },
+    { image: first, title: 'Sommerferien!', datetime: '05.07.2024 - 12.08.2024', content: 'Die Saison neigt sich dem Ende, darum wünschen wir allen Spielern und Eltern unserer Spieler einen erholsamen Sommer!' },
+  ];
 
-	const renderUpNextData = upNextData.map((event) => (
-		<Grid.Col md={4} sm={6}>
-			<Card shadow={'sm'} radius={'md'}>
-				<Card.Section>
-					<AspectRatio ratio={2048/1357}>
-						<Image
-							src={event.image}
-							height={'100%'}
-							alt={'first'}
-							fit={'fill'}
-						/>
-					</AspectRatio>
-					<Group position={'apart'} m={'md'}>
-						<Text weight={500}>{event.title}</Text>
-						<Badge color={'blue'} variant={'light'}>
-							{event.datetime}
-						</Badge>
-					</Group>
-					<Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-						{event.content}
-					</Text>
-				</Card.Section>
-			</Card>
-		</Grid.Col>
-	));
+  const renderUpNextData = upNextData.map((event) => (
+    <Grid.Col md={4} sm={6}>
+      <Card shadow={'sm'} radius={'md'}>
+        <Card.Section>
+          <AspectRatio ratio={2048 / 1357}>
+            <Image
+              src={event.image}
+              height={'100%'}
+              alt={'first'}
+              fit={'fill'}
+            />
+          </AspectRatio>
+          <Group position={'apart'} m={'md'}>
+            <Text weight={500}>{event.title}</Text>
+            <Badge color={'blue'} variant={'light'}>
+              {event.datetime}
+            </Badge>
+          </Group>
+          <Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
+            {event.content}
+          </Text>
+        </Card.Section>
+      </Card>
+    </Grid.Col>
+  ));
 
-	const sponsorData = [
-		{ image: munishi, link: 'https://munishi.ch', alternativeText: 'Munishi Logo'},
-		{ image: tgazajug, link: 'https://www.tgazajug.com', alternativeText: 'Tga Za Jug Logo'},
-		{ image: dieMobiliar, link: 'https://www.mobiliar.ch', alternativeText: 'die Mobiliar Logo'},
-		{ image: hederavita, link: 'https://hederavita.ch', alternativeText: 'Hederavita Logo'},
-		{ image: konsens, link: 'https://kon-sens.com', alternativeText: 'Konsens Logo'},
-		{ image: stilmat, link: 'https://www.stilmat.com/de/', alternativeText: 'Stilmat Logo'}
-	];
+  const sponsorData = [
+    { image: hostpoint, link: 'https://www.hostpoint.ch/', alternativeText: 'Hostpoint Logo' },
+    { image: velopeter, link: 'https://velopeter.ch/', alternativeText: 'Velopeter Logo' },
+    { image: apotheke, link: 'http://www.gate24.ch/de/Beauty+Wellness-13/Sirnach/Apotheke-Sirnach-Dr--R--Krahenmann/detail-2951248-2940210.aspx', alternativeText: 'Dr. Krähemann Apotheke Logo' },
+    { image: swissInsurance, link: 'https://www.swiss-insurance-ag.ch/', alternativeText: 'Swiss Insurance Logo' },
+    { image: raiffeisen, link: 'https://www.raiffeisen.ch/regio-sirnach/de.html', alternativeText: 'Raiffaisen Logo' },
+    { image: fitness4all, link: 'http://www.fitness4all.ch/', alternativeText: 'Fitness4All Logo' }
+  ];
 
-	const renderSponsors = sponsorData.map((sponsor) => (
-		<Grid.Col sm={6} md={4}><a href={sponsor.link}><img src={sponsor.image} alt={sponsor.alternativeText}/></a></Grid.Col>
-	))
+  const renderSponsors = sponsorData.map((sponsor) => (
+    <Grid.Col sm={6} md={4}><a href={sponsor.link}><img src={sponsor.image} alt={sponsor.alternativeText} /></a></Grid.Col>
+  ))
 
-	useEffect(() => {
-		AOS.init({ duration: 1000 })
-	});
+  useEffect(() => {
+    AOS.init({ duration: 1000 })
+  });
 
-	const autoplay = useRef(Autoplay({ delay: 6000 }));
+  const autoplay = useRef(Autoplay({ delay: 6000 }));
 
-    const useStyles = createStyles((theme) => ({
-        card: {
-        height: rem(440),
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        },
+  const useStyles = createStyles((theme) => ({
+    card: {
+      height: rem(440),
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
 
-        title: {
-        fontWeight: 900,
-        textShadow: '2px 2px #ffffff',
-        color: '#1B1A55',
-        lineHeight: 1.2,
-        fontSize: rem(50),
-        marginTop: theme.spacing.xs,
-        },
-          }));
+    title: {
+      fontWeight: 900,
+      textShadow: '2px 2px #ffffff',
+      color: '#1B1A55',
+      lineHeight: 1.2,
+      fontSize: rem(50),
+      marginTop: theme.spacing.xs,
+    },
+  }));
 
-    interface CardProps {
-      image: string;
-      title: string;
-    }
+  interface CardProps {
+    image: string;
+    title: string;
+  }
 
-    function Hero({ image, title }: CardProps) {
-      const { classes } = useStyles();
+  function Hero({ image, title }: CardProps) {
+    const { classes } = useStyles();
 
-  return (
-  <AspectRatio ratio={2/1}>
-    <Paper
-      p="xl"
-      radius={0}
-      sx={{ backgroundImage: `url(${image})`,
-      backgroundSize: '100% 100%'}}
-      className={classes.card}
-    >
-      <div>
-        <Title order={1} className={classes.title} my='xl'>
-          {title}
-        </Title>
-      </div>
-      <Button variant="white" color="dark" my='xl'>
-        Read article
-      </Button>
-    </Paper>
-  </AspectRatio>
-  );
-}
+    return (
+      <AspectRatio ratio={2 / 1}>
+        <Paper
+          p="xl"
+          radius={0}
+          sx={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: '100% 100%'
+          }}
+          className={classes.card}
+        >
+          <div>
+            <Title order={1} className={classes.title} my='xl'>
+              {title}
+            </Title>
+          </div>
+          <Button variant="white" color="dark" my='xl'>
+            Read article
+          </Button>
+        </Paper>
+      </AspectRatio>
+    );
+  }
 
-    const data = [
-  {
-    image: whole,
-    title: 'Willkommen beim BC Bears Wil!',
-  },
-  {
-    image: bear1,
-    title: 'Worauf wartest du noch? Melde dich hier an!',
-  },
-  {
-    image: bear2,
-    title: 'Geschichte',
-  },
-  {
-    image: whole,
-    title: 'shop',
-  },
+  const data = [
+    {
+      image: whole,
+      title: 'Willkommen beim BC Bears Wil!',
+    },
+    {
+      image: bear1,
+      title: 'Worauf wartest du noch? Melde dich hier an!',
+    },
+    {
+      image: bear2,
+      title: 'Geschichte',
+    },
+    {
+      image: whole,
+      title: 'shop',
+    },
   ];
 
   const slides = data.map((item) => (
@@ -176,60 +179,60 @@ function Home() {
     </Carousel.Slide>
   ));
 
-	return (
-		<Center>
-			<div>
-				<div className={'main'}>
-                    <Space h='xl'/>
-					<div className='carousel'>
-                    <Carousel slideSize={'100%'}
-								  height={'xl'}
-								  withIndicators
-								  withControls={false}
-								  plugins={[autoplay.current]}
-								  onMouseEnter={autoplay.current.stop}
-								  onMouseLeave={autoplay.current.reset}
-								  loop
-								  styles={{
-									  indicator: {
-										  width: rem(12),
-										  height: rem(4),
-										  transition: 'width 250ms ease',
+  return (
+    <Center>
+      <div>
+        <div className={'main'}>
+          <Space h='xl' />
+          <div className='carousel'>
+            <Carousel slideSize={'100%'}
+              height={'xl'}
+              withIndicators
+              withControls={false}
+              plugins={[autoplay.current]}
+              onMouseEnter={autoplay.current.stop}
+              onMouseLeave={autoplay.current.reset}
+              loop
+              styles={{
+                indicator: {
+                  width: rem(12),
+                  height: rem(4),
+                  transition: 'width 250ms ease',
 
-										  '&[data-active]': {
-											  width: rem(40),
-										  },
-									  },
-								  }}
-                        >
-                          {slides}
-                    </Carousel>
-					</div>
-                    <div className='next'>
-					<div className={'next-title'}>
-						<Title order={1} align={'center'} mb={'xl'} c='white' pt='xl'>Wichtige Termine</Title>
-					</div>
-                        <div className='next-description'>
-                        <Text c='white' mx='7%' mb='xl' size='xl'>Hier finden unsere Spieler und deren Eltern die nächsten wichtigen Termine. Von Spezialtrainings bis hin zu geplanten Trainingsausfällen durch andersweitige Hallenbesetzungen findet Ihr hier alles. Diese Sammlung wird regelmässig gepflegt, sodass ihr sicherlich keine wichtigen Infos verpasst. Bei Unklarheiten könnt ihr euch jederzeit bei einem der Trainer auf Whatsapp melden.</Text>
-                        </div>
-					<div className={'next-content'}>
-						<Grid justify={'center'} align={'center'} mx={'lg'} pb='xl'>
-							{renderUpNextData}				
-						</Grid>
-					</div>
-                    </div>
-                    <Divider my='xl' mx='7%'/>
-					<div className={'sponsors-title'}>
-						<Title order={1} align={'center'} my={'xl'}>Unsere Sponsoren</Title>
-					</div>
-					<div className='sponsor-images grid'>
-							<Grid justify='center' align='center' pb={'xl'}>
-								{renderSponsors}
-							</Grid>
-					</div>
-				</div>
-			</div>
-		</Center>
-	);
+                  '&[data-active]': {
+                    width: rem(40),
+                  },
+                },
+              }}
+            >
+              {slides}
+            </Carousel>
+          </div>
+          <div className='next'>
+            <div className={'next-title'}>
+              <Title order={1} align={'center'} mb={'xl'} c='white' pt='xl'>Wichtige Termine</Title>
+            </div>
+            <div className='next-description'>
+              <Text c='white' mx='7%' mb='xl' size='xl'>Hier finden unsere Spieler und deren Eltern die nächsten wichtigen Termine. Von Spezialtrainings bis hin zu geplanten Trainingsausfällen durch andersweitige Hallenbesetzungen findet Ihr hier alles. Diese Sammlung wird regelmässig gepflegt, sodass ihr sicherlich keine wichtigen Infos verpasst. Bei Unklarheiten könnt ihr euch jederzeit bei einem der Trainer auf Whatsapp melden.</Text>
+            </div>
+            <div className={'next-content'}>
+              <Grid justify={'center'} align={'center'} mx={'lg'} pb='xl'>
+                {renderUpNextData}
+              </Grid>
+            </div>
+          </div>
+          <Divider my='xl' mx='7%' />
+          <div className={'sponsors-title'}>
+            <Title order={1} align={'center'} my={'xl'}>Unsere Sponsoren</Title>
+          </div>
+          <div className='sponsor-images grid'>
+            <Grid justify='center' align='center' pb={'xl'}>
+              {renderSponsors}
+            </Grid>
+          </div>
+        </div>
+      </div>
+    </Center>
+  );
 }
 export default Home;
--- src/pages/Verein/Trainer/index.tsx
+++ src/pages/Verein/Trainer/index.tsx
@@ -1,17 +1,17 @@
 // Mantine imports
 import {
-    Title,
-    Image,
-    Group,
-    Stack,
-    ThemeIcon,
-    Space,
-    Text,
-    Badge,
-    Box,
-    AspectRatio,
-    Grid,
-    Card
+  Title,
+  Image,
+  Group,
+  Stack,
+  ThemeIcon,
+  Space,
+  Text,
+  Badge,
+  Box,
+  AspectRatio,
+  Grid,
+  Card
 } from '@mantine/core'
 
 // Image imports
@@ -21,55 +21,55 @@ import placeholder from '../../../media/placeholder_person.jpeg'
 // Icon imports
 import { Phone, Mail } from 'tabler-icons-react'
 
-function Trainer () {
-    const trainers = [
+function Trainer() {
+  const trainers = [
     { image: davor, name: 'Davor', selection: 'Knaben/Mädchen', email: 'd.lucic4@hotmail.com', tel: '+41791795801' },
     { image: placeholder, name: 'Thomas', selection: 'Knaben/Mädchen', email: '*email*', tel: '*tel*' },
-    ]
+  ]
 
-    const renderTrainers = trainers.map((trainer) => (
-        <Grid.Col sm={6} md={4} xl={3}>
-            <Card shadow='md' radius='md' mb='xl'>
-                <Card.Section>
-                    <AspectRatio ratio={6/8}>
-                        <Image
-                            src={trainer.image}
-                        />
-                    </AspectRatio>
-                    <Group position='apart' mx='md' my='xs'>
-                        <Title c='#1B1A55' order={2}>{trainer.name}</Title>
-                        <Badge color='blue' variant='light'>{trainer.selection}</Badge>
-                    </Group>
-                    <Stack spacing='xs' mt='lg'>
-                        <Text c='dimmed' component='a' href={`mailto:${trainer.email}`} mx='lg'><ThemeIcon mx='sm' color='blue' variant='light'><Mail /></ThemeIcon>{trainer.email}</Text>
-                        <Text c='dimmed' component='a' href={`tel:${trainer.tel}`} mx='lg' mb='md'><ThemeIcon mx='sm' color='blue' variant='light'><Phone /></ThemeIcon>{trainer.tel}</Text>
-                    </Stack>
-                </Card.Section>
-            </Card>
-        </Grid.Col>
-        ));
+  const renderTrainers = trainers.map((trainer) => (
+    <Grid.Col sm={6} md={4} xl={3}>
+      <Card shadow='md' radius='md' mb='xl'>
+        <Card.Section>
+          <AspectRatio ratio={6 / 8}>
+            <Image
+              src={trainer.image}
+            />
+          </AspectRatio>
+          <Group position='apart' mx='md' my='xs'>
+            <Title c='#1B1A55' order={2}>{trainer.name}</Title>
+            <Badge color='blue' variant='light'>{trainer.selection}</Badge>
+          </Group>
+          <Stack spacing='xs' mt='lg'>
+            <Text c='dimmed' component='a' href={`mailto:${trainer.email}`} mx='lg'><ThemeIcon mx='sm' color='blue' variant='light'><Mail /></ThemeIcon>{trainer.email}</Text>
+            <Text c='dimmed' component='a' href={`tel:${trainer.tel}`} mx='lg' mb='md'><ThemeIcon mx='sm' color='blue' variant='light'><Phone /></ThemeIcon>{trainer.tel}</Text>
+          </Stack>
+        </Card.Section>
+      </Card>
+    </Grid.Col>
+  ));
 
-    return (
-        <div>
-            <div className="main">
-                <Space h='xl'/>
-                <Space h='xl'/>
-                <Box mx={'7%'} mt={'4%'} sx={() => ({
-                    backgroundColor: '#1B1A55',
-                    borderRadius: '7px'
-                })}>
-                    <Title order={1} mt={'xl'} mx={'xl'} c={'white'}>
-                        Trainer 
-                    </Title>
-                </Box>
-                <div className='trainers-grid'>
-                    <Grid gutter={30} mx='8%' my={'xl'}>
-                    {renderTrainers}
-                    </Grid>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="main">
+        <Space h='xl' />
+        <Space h='xl' />
+        <Box mx={'7%'} mt={'4%'} sx={() => ({
+          backgroundColor: '#1B1A55',
+          borderRadius: '7px'
+        })}>
+          <Title order={1} mt={'xl'} mx={'xl'} c={'white'}>
+            Trainer
+          </Title>
+        </Box>
+        <div className='trainers-grid'>
+          <Grid gutter={30} mx='8%' my={'xl'}>
+            {renderTrainers}
+          </Grid>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Trainer;
