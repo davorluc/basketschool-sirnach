efee90819fea965a588d0c2ffef5f8eaa0fa1f2a
davorluc <d.lucic4@hotmail.com>
2024-09-03 15:30:19 +0200

    indentation

--- src/App.tsx
+++ src/App.tsx
@@ -13,22 +13,22 @@ import Contact from "./pages/Contact";
 
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="infos/" element={<Verein />} />
-                    <Route path="infos/geschichte" element={<Geschichte />} />
-                    <Route path="infos/vorstand" element={<Vorstand />} />
-                    <Route path="infos/trainer" element={<Trainer />} />
-                    <Route path="infos/hallen" element={<Hallen />} />
-                    <Route path="infos/neumitglied" element={<Neumitglied />} />
-                    <Route path="partners/" element={<Partners />} />
-                    <Route path="kontakt/" element={<Contact />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="infos/" element={<Verein />} />
+          <Route path="infos/geschichte" element={<Geschichte />} />
+          <Route path="infos/vorstand" element={<Vorstand />} />
+          <Route path="infos/trainer" element={<Trainer />} />
+          <Route path="infos/hallen" element={<Hallen />} />
+          <Route path="infos/neumitglied" element={<Neumitglied />} />
+          <Route path="partners/" element={<Partners />} />
+          <Route path="kontakt/" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 
-    );
+  );
 }
