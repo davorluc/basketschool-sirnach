9f7de0df7ac3bf1b790662b03bee3adbba84c776
davorluc <d.lucic4@hotmail.com>
2024-09-03 15:10:31 +0200

    Updated Club Logo
    Created new logo for "Verein" Page and rest of website

--- src/pages/Verein/index.tsx
+++ src/pages/Verein/index.tsx
@@ -3,67 +3,67 @@ import React from "react";
 
 // Mantine Imports
 import {
-    Grid,
-    Image,
-    AspectRatio,
-    Space,
-    Card,
-    Title,
+  Grid,
+  Image,
+  AspectRatio,
+  Space,
+  Card,
+  Title,
 } from '@mantine/core';
 
 // Stylesheets Import
 import './index.css';
 
 // Image Imports
-import geschichte from '../../media/wil_basketball_small_cropped.png';
+import geschichte from '../../media/basketschool_sirnach_logo.png';
 // import vorstand from
 // import trainer from
 // import hallen from
 // import mitglied from
 
-function Verein () {
-    const vereinData = [
-        { image: geschichte, title: 'Geschichte', link: '/infos/geschichte' },
-        { image: geschichte, title: 'Vorstand', link: '/infos/vorstand' },
-        { image: geschichte, title: 'Trainer', link: 'infos/trainer' },
-        { image: geschichte, title: 'Wann & Wo', link: '/infos/hallen' },
-        { image: geschichte, title: 'Mitglied werden', link: '/infos/neumitglied'}
-    ];
+function Verein() {
+  const vereinData = [
+    { image: geschichte, title: 'Geschichte', link: '/infos/geschichte' },
+    { image: geschichte, title: 'Vorstand', link: '/infos/vorstand' },
+    { image: geschichte, title: 'Trainer', link: 'infos/trainer' },
+    { image: geschichte, title: 'Wann & Wo', link: '/infos/hallen' },
+    { image: geschichte, title: 'Mitglied werden', link: '/infos/neumitglied' }
+  ];
 
-    const renderVereinData = vereinData.map((entry) => (
-        <Grid.Col md={4} sm={6}>
-            <Card shadow={'sm'} radius={'md'} style={{backgroundColor: "#F0F0F0"}} component={'a'} href={entry.link}>
-                <Card.Section p={'xl'}>
-                    <AspectRatio ratio={2000/1797} p={'xl'}>
-                        <Image
-                            src={entry.image}
-                            height={'100%'}
-                            alt={'first'}
-                            fit={'fill'}
-                        />
-                    </AspectRatio>
-                    <Title m={'xl'} p={'xl'} align={'center'} order={2}>{entry.title}</Title>
-                </Card.Section>
-            </Card>
-        </Grid.Col>
-    ))
+  const renderVereinData = vereinData.map((entry) => (
+    <Grid.Col md={4} sm={6}>
+      <Card shadow={'sm'} radius={'md'} style={{ backgroundColor: "#F0F0F0" }} component={'a'} href={entry.link}>
+        <Card.Section p={'xl'}>
+          <AspectRatio ratio={2000 / 1797} p={'xl'}>
+            <Image
+              src={entry.image}
+              height={'100%'}
+              alt={'first'}
+              fit={'fill'}
+            />
+          </AspectRatio>
+          <Title m={'xl'} p={'xl'} align={'center'} order={2}>{entry.title}</Title>
+        </Card.Section>
+      </Card>
+    </Grid.Col>
+  ))
 
-    return (
-        <div>
-            <div className={'main'}>
-                <Space h={'xl'}/>
-                <Space h={'xl'}/>
-                <div className={'header'}>
-                    <Title order={1} my={'xl'} mx={'7%'} c='#189940' align={'left'}>Verein</Title>
-                </div>
-                <div className={'verein-grid'}>
-                    <Grid my={'xl'} align={'center'} justify={'center'} mx={'xl'} gutter={30}>
-                        {renderVereinData}
-                    </Grid>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className={'main'}>
+        <Space h={'xl'} />
+        <Space h={'xl'} />
+        <div className={'header'}>
+          <Title order={1} my={'xl'} mx={'7%'} c='#1B1A55' align={'left'}>Verein</Title>
         </div>
-    );
+        <div className={'verein-grid'}>
+          <Grid my={'xl'} align={'center'} justify={'center'} mx={'xl'} gutter={30}>
+            {renderVereinData}
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Verein;
