d277d34539b0a91aa2b72091ff492aa338e29213
davorluc <d.lucic4@hotmail.com>
2024-11-07 16:54:18 +0100

    text for each major sponsor on sponsor page

--- src/pages/Partners/index.tsx
+++ src/pages/Partners/index.tsx
@@ -25,12 +25,12 @@ import './index.css';
 
 function Partners() {
   const mainSponsor = [
-    { image: hostpoint, description: 'Sponsorbeschreibung', name: 'Hostpoint', link: 'https://www.hostpoint.ch' },
-    { image: velopeter, description: 'Sponsorenbeschreibung', name: 'Velopeter', link: 'https://velopeter.ch/' },
-    { image: apotheke, description: 'Sponsorenbeschreibung', name: 'Apotheke Krähenmann', link: 'http://www.gate24.ch/de/Beauty+Wellness-13/Sirnach/Apotheke-Sirnach-Dr--R--Krahenmann/detail-2951248-2940210.aspx' },
-    { image: swissInsurance, description: 'Sponsorenbeschreibung', name: 'Swiss Insurance AG', link: 'https://www.swiss-insurance-ag.ch/' },
-    { image: raiffeisen, description: 'Sponsorenbeschreibung', name: 'Raiffeisen', link: 'https://www.raiffeisen.ch/regio-sirnach/de.html' },
-    { image: fitness4all, description: 'Sponsorenbeschreibung', name: 'Fitness4all', link: 'http://www.fitness4all.ch/' }
+    { image: hostpoint, description: 'Der führende Schweizer Web-hosting Anbieter und ausserdem unser Partner für alles rund ums Hosting. Eine Portion Hosting gefällig? - Mit Hostpoint macht man nichts falsch!', name: 'Hostpoint', link: 'https://www.hostpoint.ch' },
+    { image: velopeter, description: 'Velopeter - langjähriger Fahrrad-Spezialist in Sirnach. Wir empfehlen unseren Spielern falls möglich zu Fuss oder mit dem Velo zu kommen. Beim Velo kann euch der Velopeter sicher aushelfen!', name: 'Velopeter', link: 'https://velopeter.ch/' },
+    { image: apotheke, description: 'Langjährige Apotheke des Vertrauens in Sirnach, auch perfekt geeignet für Mittel gegen Sportverletzungen. Knöchel umgeknickst? Kein Problem, die Apotheke Krähenmann regelt das schon!', name: 'Apotheke Krähenmann', link: 'http://www.gate24.ch/de/Beauty+Wellness-13/Sirnach/Apotheke-Sirnach-Dr--R--Krahenmann/detail-2951248-2940210.aspx' },
+    { image: swissInsurance, description: 'Seit 20 Jahren Versicherungsanbieter im Thurgau. Egal ob Versicherung, Pensionskasse oder Pensions Planung, Hypotheken, Beratungen und Krankenkasse, bei Swiss Insurance ist sicher was für alle dabei!', name: 'Swiss Insurance AG', link: 'https://www.swiss-insurance-ag.ch/' },
+    { image: raiffeisen, description: 'Seit über 100 Jahren national wie auch lokal gut vertreten. 1900 schon eine Genossenschaft, 2024 noch eine Genosschenschaft. Die Raiffeisen Bank ist unsere Bank des Vertrauens. ', name: 'Raiffeisen', link: 'https://www.raiffeisen.ch/regio-sirnach/de.html' },
+    { image: fitness4all, description: 'Auch der Kraftraum gehört zum Arsenal eines Basketballers. Mehr Sprungkraft, schnellerer Sprint und stabilere Gelenke gehören auch zum Besserwerden. Schaut doch mal bei unserem Fitnesspartner vorbei!', name: 'Fitness4all', link: 'http://www.fitness4all.ch/' }
   ];
 
   const mainSponsorContent = mainSponsor.map((entry) => (
