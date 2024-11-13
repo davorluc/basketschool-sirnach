70e85a95655cc5b274292b528eb45eea9a8133b1
davorluc <d.lucic4@hotmail.com>
2024-11-13 23:32:34 +0100

    added additional links to sponsors page

--- src/pages/Partners/index.tsx
+++ src/pages/Partners/index.tsx
@@ -10,6 +10,7 @@ import {
   Title,
   Box,
   Divider,
+  Spoiler,
   Image
 } from '@mantine/core'
 
@@ -18,6 +19,10 @@ import hostpoint from '../../media/sponsors/logo_hostpoint_orig.png';
 import velopeter from '../../media/sponsors/logo_velopeter_ohne_weiss.png';
 import swissInsurance from '../../media/sponsors/Logo_swissinsuranceag_04.png';
 import cubeless from '../../media/sponsors/cubeless_logo_rgb_500px_transp Juli 2023.png';
+import bcbears from '../../media/teams/u14.jpeg';
+import frauenfeld from '../../media/frauenfeld.jpg';
+import swissbasket from '../../media/Swiss_Basketball_logo.png';
+import sbb from '../../media/sbb.png';
 
 import './index.css';
 
@@ -29,23 +34,59 @@ function Partners() {
     { image: cubeless, description: 'Auf cubeless.ch erwartet dich ein ausgesuchtes Sortiment an verschiedensten Speedcubes, Shape Mods und passendem Zubehör. Da ist auf jeden Fall der passende Speedcube für dich dabei, egal ob du Anfänger oder Profi bist.', name: 'Cubeless', link: 'https://www.cubeless.ch/' }
   ];
 
+  const otherRefsData = [
+    { image: bcbears, description: 'Unser guter Freund aus dem nahegelegenden Wil. Als einer der grösseren Vereine in der Region bieten sie vom regulärem Schulalter bis zu den Senioren alles an.', name: 'BC Bears Wil', link: 'https://bcbears.ch/' },
+    { image: frauenfeld, description: 'Seit Anbeginn der Basketschool Sirnach war der Basketball Club in Frauenfeld eines der Vereine unseres Vertrauen. CVJM Frauenfeld ist schon jahrelanger Basketball-Riese der Ostschweiz.', name: 'CVJM Frauenfeld', link: 'https://basket-frauenfeld.ch/home' },
+    { image: swissbasket, description: 'Der offizielle Schweizer Basketballverbund - Hier findet ihr alles um den Schweizer Basketball: von Events zu Informationen. Schaut doch mal rein!', name: 'Swiss Basketball', link: 'https://swiss.basketball/' },
+    { image: sbb, description: 'Für unsere Spieler die etwas weiter weg wohnen und mit dem ÖV anreisen müssen, empfehlen wir euch bei der SBB zu erkundigen. Egal ob per Webseite oder per App, so findest du bestimmt zu uns!', name: 'SBB', link: 'https://www.sbb.ch/de' },
+  ];
+
   const mainSponsorContent = mainSponsor.map((entry) => (
     <Grid.Col md={4} sm={6}>
-      <Card shadow={'sm'} radius={'md'} component={'a'} href={entry.link}>
+      <Card shadow={'sm'} radius={'md'}>
         <Card.Section my={'xl'}>
           <AspectRatio ratio={35 / 12}>
             <Image
               src={entry.image}
-              fit={'contain'}
+              fit={'fill'}
               p={'xl'}
+              component={'a'}
+              href={entry.link}
             />
           </AspectRatio>
           <Title order={3} mx={'xl'}>
             {entry.name}
           </Title>
-          <Text size={'sm'} color={'dimmed'} mx={'lg'} maw={'60ch'} my={'sm'}>
-            {entry.description}
-          </Text>
+          <Spoiler showLabel={'mehr'} hideLabel={'weniger'} maxHeight={50} mx={'lg'}>
+            <Text size={'sm'} color={'dimmed'} my={'md'}>
+              {entry.description}
+            </Text>
+          </Spoiler>
+        </Card.Section>
+      </Card>
+    </Grid.Col>
+  ));
+
+  const otherRefs = otherRefsData.map((entry) => (
+    <Grid.Col md={4} sm={6}>
+      <Card shadow={'sm'} radius={'md'}>
+        <Card.Section my={'xl'}>
+          <AspectRatio ratio={35 / 15}>
+            <Image
+              src={entry.image}
+              fit={'fill'}
+              component={'a'}
+              href={entry.link}
+            />
+          </AspectRatio>
+          <Title order={3} mx={'xl'} mt={'md'}>
+            {entry.name}
+          </Title>
+          <Spoiler showLabel={'mehr'} hideLabel={'weniger'} maxHeight={50} mx={'lg'}>
+            <Text size={'sm'} color={'dimmed'} my={'sm'}>
+              {entry.description}
+            </Text>
+          </Spoiler>
         </Card.Section>
       </Card>
     </Grid.Col>
@@ -70,15 +111,17 @@ function Partners() {
         </Box>
         <div className='main-sponsors'>
           <Title order={1} c='#1B1A55' mx='8%'>Hauptsponsoren</Title>
-          <Grid mx={'xl'} mt={'xl'} className={'main-sponsors-grid'}>
+          <Grid mx={'xl'} mt={'xl'} className={'sponsors-grid'}>
             {mainSponsorContent}
           </Grid>
         </div>
         <Divider m='xl' />
         <div className='main-sponsors'>
-          <Title order={1} c='#1B1A55' mx='8%'>Andere Partner und Links</Title>
+          <Title order={1} c='#1B1A55' mx='8%'>Nützliche Links</Title>
+          <Grid mx={'xl'} my={'xl'} className={'sponsors-grid'}>
+            {otherRefs}
+          </Grid>
         </div>
-        <Divider m='xl' />
       </div>
     </div>
   );
